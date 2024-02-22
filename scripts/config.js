7e7e4e9d7256b84a7ed21da83e9500255470a428
Maks_Skabitski <ska6itskiy@gmail.com>
2024-02-22 19:25:04 +0200

    added some JS code

---
+++ scripts/config.js
@@ -0,0 +1,20 @@
+function openPlayerConfig() {
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get("playername").trim();
+  console.log(enteredPlayername)
+  if (!enteredPlayername) {
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
+}
