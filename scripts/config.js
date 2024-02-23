cf180fe9129bccb49d40a1866a770d44f921f286
Maks_Skabitski <ska6itskiy@gmail.com>
2024-02-23 15:03:26 +0200

    added some logic for players

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,6 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  const selectedPlayerId = event.target.dataset.playerid;
+  editedPlayer = selectedPlayerId
   playerConfigOverlayElement.style.display = "block";
   backdropElement.style.display = "block";
 }
@@ -6,15 +8,32 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = ''
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
   const enteredPlayername = formData.get("playername").trim();
-  console.log(enteredPlayername)
+  console.log(enteredPlayername);
+
   if (!enteredPlayername) {
+    event.target.firstElementChild.classList.add("error");
     errorsOutputElement.textContent = "Please enter a valid name!";
     return;
   }
+
+  const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data')
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername
+
+//   if (editedPlayer ===1 ){
+//     player[0].name = enteredPlayername
+// } else {
+//   player[1].name = enteredPlayername
+// }
+
+players[editedPlayer - 1].name = enteredPlayername
+closePlayerConfig()
 }
